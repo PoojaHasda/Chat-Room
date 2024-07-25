e274d4519e3117f1798726cd6a30ebaafd1c64b8
PoojaHasda <141259541+PoojaHasda@users.noreply.github.com>
2024-07-25 11:50:29 +0530

    first commit

---
+++ src/main/java/com/chatApp/chatRoomApp/ChatRoomAppApplication.java
@@ -0,0 +1,14 @@
+package com.chatApp.chatRoomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatRoomAppApplication.class, args);
+	}
+
+}
